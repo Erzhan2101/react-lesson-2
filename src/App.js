03303f2b7f30da1843b0993b35641d1e3859b641
Erzhan2101 <erzhan210198@gmail.com>
2021-08-18 16:57:31 +0600

    new-proj

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css'
+import Header from "./Header";
+
+
+function App() {
+    return (
+        <div>
+                <Header />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div>
+            <nav className="nav-bar">
+                <ul><a href="#">Доставка</a></ul>
+                <ul><a href="#">Оплата</a></ul>
+                <ul><a href="#">Отзывы</a></ul>
+                <ul><a href="#">Вопрос - ответ</a></ul>
+                <ul><a href="#">Контакты</a></ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
